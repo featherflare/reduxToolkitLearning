49a2910224d09bc5527ef4c339d4d123cd27b30f
max43607 <max43607@gmail.com>
2023-05-10 10:47:17 +0700

    Lesion _02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Counter from "./features/counter/Counter";
+import AddPostForm from './features/posts/AddPostForm'
+import PostsList from './features/posts/PostsList'
 
 function App() {
   return (
-    <main className="App">
-      <Counter />
+    <main className='App'>
+      <AddPostForm />
+      <PostsList />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
