7fb80d1fd8773a7fe443c475eb3ecde979399eae
max43607 <max43607@gmail.com>
2023-05-11 11:42:37 +0700

    Lession _06 & Set up Lession 07

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import PostsList from './features/lession_05/posts/PostsList'
-import AddPostForm from './features/lession_05/posts/AddPostForm'
-import SinglePostPage from './features/lession_05/posts/SinglePostPage'
-import EditPostForm from './features/lession_05/posts/EditPostForm'
-import UsersList from './features/users/UserList'
-import UserPage from './features/users/UserPage'
-import Layout from './components/Layout'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from './features/users/UserPage';
+import Layout from "./components/Layout";
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path="/" element={<Layout />}>
+
         <Route index element={<PostsList />} />
 
-        <Route path='post'>
+        <Route path="post">
           <Route index element={<AddPostForm />} />
-          <Route path=':postId' element={<SinglePostPage />} />
-          <Route path='edit/:postId' element={<EditPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
 
-        <Route path='user'>
+        <Route path="user">
           <Route index element={<UsersList />} />
-          <Route path=':userId' element={<UserPage />} />
+          <Route path=":userId" element={<UserPage />} />
         </Route>
 
         {/* Catch all - replace with 404 component if you want */}
-        <Route path='*' element={<Navigate to='/' replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
